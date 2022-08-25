f8939ad2271368810cfa17d841152f77f160bb02
Inieke Sendy <ini.e.sendy@gmail.com>
2022-08-25 18:19:58 +0100

    [f-structure]: Update HTML for JS Dom

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import "./style.css";
+import PageTitle from "./components/PageTitle";
+import MainBody from "./components/MainBody.js";
+import Footer from "./components/Footer.js";
 
-const component = () => {
-  const element = document.createElement("div");
+console.log(MainBody());
 
-  element.innerHTML = "Hello World";
-
-  return element;
-};
-
-document.body.appendChild(component());
+document.body.appendChild(PageTitle());
+document.body.appendChild(MainBody());
