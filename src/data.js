96589ee62001186e2092516ebf7306fba3f5effc
Inieke Sendy <ini.e.sendy@gmail.com>
2022-08-25 18:19:24 +0100

    [f-structure]: Add Data.js

---
+++ src/data.js
@@ -0,0 +1,60 @@
+const dataset = [
+  {
+    name: "Peters Asuquo",
+    score: 100,
+  },
+  {
+    name: "Peters Asuquo",
+    score: 101,
+  },
+  {
+    name: "Peters Asuquo",
+    score: 102,
+  },
+  {
+    name: "Peters Asuquo",
+    score: 103,
+  },
+  {
+    name: "Peters Asuquo",
+    score: 104,
+  },
+  {
+    name: "Peters Asuquo",
+    score: 105,
+  },
+  {
+    name: "Peters Asuquo",
+    score: 106,
+  },
+  {
+    name: "Peters Asuquo",
+    score: 107,
+  },
+  {
+    name: "Peters Asuquo",
+    score: 109,
+  },
+  {
+    name: "Peters Asuquo",
+    score: 104,
+  },
+  {
+    name: "Peters Asuquo",
+    score: 105,
+  },
+  {
+    name: "Peters Asuquo",
+    score: 106,
+  },
+  {
+    name: "Peters Asuquo",
+    score: 107,
+  },
+  {
+    name: "Peters Asuquo",
+    score: 109,
+  },
+];
+
+export default dataset;
