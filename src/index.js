ec23db21886a4ce14254f3d1622eb779dc4bbf47
Inieke Sendy <ini.e.sendy@gmail.com>
2022-08-26 10:19:26 +0100

    [f-structure]: Add header to HTML to Sove Lighthouse issues

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import './style.css';
-import PageTitle from './components/PageTitle.js';
 import MainBody from './components/MainBody.js';
 import Footer from './components/Footer.js';
 
-document.body.appendChild(PageTitle());
 document.body.appendChild(MainBody());
 document.body.appendChild(Footer());
